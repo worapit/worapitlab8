ae20086bb64eeed752fdfae16df5381dc633e660
worapit <drm.wmwork@gmail.com>
2021-12-01 21:31:41 +0700

    complete

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    
+    double a  =  20, b = 30;
+    double c = 4, outpot;
 
     outpot = a - b/c;
-    count << output: << output << \n;
+    cout << "output: " << outpot << "\n" ;
     return 0;
 }
